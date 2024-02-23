8425dbe8e5309fef181d16fb42c6493754d14c25
Aaron <aarongraybill3@gmail.com>
2024-02-22 20:46:46 -0600

    feat: successfull connection to db

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,8 +3,9 @@ use openai_dive::v1::{api::Client, models::Gpt35Engine, resources::chat::ChatMes
 use openai_dive::v1::resources::chat::{ChatCompletionParameters, ChatMessage, Role};
 use recipe::Recipe;
 use serde::{Deserialize, Serialize};
+use sqlx::{query, PgPool, Pool, Postgres, Row};
 use std::env;
-use actix_web::{get, web::Json, App, HttpServer, Responder};
+use actix_web::{get, web::{Data, Json}, App, HttpServer, Responder};
 
 mod recipe;
 
@@ -14,8 +15,12 @@ struct RecipeBody {
 }
 
 #[get("/recipe")]
-async fn get_recipe(body: Json<RecipeBody>) -> impl Responder {
+async fn get_recipe(body: Json<RecipeBody>, db_pool: Data<Pool<Postgres>>) -> impl Responder {
     let api_key = env::var("OPENAI_API_KEY").expect("$OPENAI_API_KEY is not set");
+
+    let res = query("SELECT * from recipe").fetch_one(&**db_pool).await.unwrap();
+    let name: String = res.get("name");
+    println!("{}", name);
 
     let client = Client::new(api_key);
 
@@ -40,8 +45,11 @@ async fn get_recipe(body: Json<RecipeBody>) -> impl Responder {
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    HttpServer::new(|| {
-        App::new().service(get_recipe)
+    let db_url = env::var("DB_URL").expect("DB_URL env not set");
+    let pool = PgPool::connect(&db_url).await.unwrap();
+    HttpServer::new(move || {
+        App::new().app_data(Data::new(pool.clone()))
+            .service(get_recipe)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
